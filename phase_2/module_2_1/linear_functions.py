4ddc9ff47b2ed1f71efa9fbb0c3ea9bcb666e88b
Temitayo Oyekunle <temitayoeman7@gmail.com>
2026-06-24 21:56:32 +0100

    plot linear and quadratic functions with matplotlib

--- phase_2/module_2_1/linear_functions.py
+++ phase_2/module_2_1/linear_functions.py
@@ -6,15 +6,17 @@ def g(x):
 def y(x):
     return 1+x
 def z(x):
-    return 2*x +13
+    return 2*x + 10
 x_values = list(range(-10, 11))
 y_values = [f(x) for x in x_values]
-y_2values=[z(x) for x in x_values]
+y_2values=[g(x) for x in x_values]
 y_3values=[y(x) for x in x_values]
+y_4values=[z(x) for x in x_values]
 
-plt.plot(x_values, y_values,label="numbrs in respect to y")
-plt.plot(x_values,y_2values,label="squared numbers")
+plt.plot(x_values, y_values,label="3*x +5")
+plt.plot(x_values,y_2values,label="x*2")
 plt.plot(x_values,y_3values,label="x+1")
+plt.plot(x_values,y_4values,label="2*x+10")
 plt.xlabel("x")
 plt.ylabel("fx")
 plt.grid(True)
